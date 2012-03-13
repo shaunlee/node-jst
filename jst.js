081f91548b791565f836f5598d36553f13b285bd
Shaun <shonhen@gmail.com>
2012-03-13 16:09:46 +0800

    move filters from window to jst

--- jst.js
+++ jst.js
@@ -4,51 +4,49 @@
  * MIT Licensed
  */
 
-// filters
-
-var filters = {};
-
-;(function(exports) {
-
-  var htmlCodes = {'&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;'},
-      htmlre = /&(?!\w+;)|<|>|"/g,
-      htmlEscape = function (src) { return htmlCodes[src]; },
-      linere = /(\r\n|\r|\n)/g;
-
-  exports.convert = function(src) {
-    return src.split('|').reduce(function(varname, filter) {
-      return 'filters.' + filter + '(' + varname + ')';
-    });
-  }
-
-  exports.e = exports.escape = function(src) {
-    return typeof src !== 'string' ? src : src.replace(htmlre, htmlEscape);
-  }
-
-  exports.linebreaks = function(src) {
-    return '<p>' + src.split(/\r\n|\n/g).join('</p><p>') + '</p>';
-  }
-
-  exports.linebreaksbr = function(src) {
-    return src.replace(linere, '<br>$1');
-  }
-
-  exports.add = function(value) {
-    return function(src) { return Number(value) + Number(src); };
-  }
-
-})(filters);
-
 // compiler
 
 var jst = {};
 
 ;(function(exports) {
 
-  var _cache = {},
+  var htmlCodes = {'&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;'},
+      htmlre = /&(?!\w+;)|<|>|"/g,
+      htmlEscape = function (src) { return htmlCodes[src]; },
+      linere = /(\r\n|\r|\n)/g,
+      //
+      _cache = {},
       _options = {
         useIt: false
       };
+
+  var filters = exports.filters = {};
+
+  (function(exports) {
+
+    exports.convert = function(src) {
+      return src.split('|').reduce(function(varname, filter) {
+        return 'filters.' + filter + '(' + varname + ')';
+      });
+    }
+
+    exports.e = exports.escape = function(src) {
+      return typeof src !== 'string' ? src : src.replace(htmlre, htmlEscape);
+    }
+
+    exports.linebreaks = function(src) {
+      return '<p>' + src.split(/\r\n|\n/g).join('</p><p>') + '</p>';
+    }
+
+    exports.linebreaksbr = function(src) {
+      return src.replace(linere, '<br>$1');
+    }
+
+    exports.add = function(value) {
+      return function(src) { return Number(value) + Number(src); };
+    }
+
+  })(exports.filters);
 
   exports.addFilter = function(name, fn) {
     filters[name] = fn;
